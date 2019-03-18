1d2872c3f5264bdeeb8a77bc779141ab54e234ef
stefano_campese_omnys <campydj90133>
2019-03-18 08:11:27 +0100

    fix and update for first assignment

--- src/main/java/G03HM1.java
+++ src/main/java/G03HM1.java
@@ -19,9 +19,24 @@ public class G03HM1 {
         ArrayList<Double> lNumbers = new ArrayList<>();
         Scanner s =  new Scanner(new File(args[0]));
         while (s.hasNext()){
-            lNumbers.add(Double.parseDouble(s.next()));
+            try {
+                double val = Double.parseDouble(s.next());
+                // Add 'val' to the ArrayList only if is non-negative
+                if (val >= 0) {
+                    lNumbers.add(val);
+                }
+            }
+            catch (Exception e){
+                System.out.println("L'elemento che si vuole aggiungere ai dati non è un numero.");
+            }
         }
         s.close();
+
+        if(lNumbers.isEmpty()){
+            throw new IllegalArgumentException("To deal with this assigment you need to have at least one element");
+        }
+
+        DoubleComparator comparator = new DoubleComparator();
 
         // Setup Spark
         SparkConf conf = new SparkConf(true)
@@ -32,42 +47,77 @@ public class G03HM1 {
         JavaRDD<Double> dNumbers = sc.parallelize(lNumbers);
 
         /**
-         * STEP 1 getting max values in two ways
+         * STEP 1 get the max value in two ways:
+         * In first way we obtain the max value through the usage of the reduce function that takes a lambda function as argument. The lambda function compares two elements and returns the biggest one.
+         * In the second method we used the max method of RDD interface with a custom Comparator that compare two numbers and that is used by the max function to compute the max value.
+         */
+
+        /**
+         * This value is the max value calculated in the first way as previously described
          */
         System.out.println("Max number is: "+String.valueOf(dNumbers.reduce((x, y) -> {return (x >= y?x:y); })));
-        System.out.println("Max number is: "+String.valueOf(dNumbers.max(new DoubleComparator())));
+
+        /**
+         * This value is the max value calculated in the second way as previously described
+         */
+        System.out.println("Max number is: "+String.valueOf(dNumbers.max(comparator)));
 
 
         /**
-         * STEP 2 getting a normalized version of dNumbers (0,1)
-         * normalized = (x-min(x))/(max(x)-min(x))
+         * STEP 2 get a normalized version of dNumbers (0,1)
+         * For this step of the assignment, we have used the map function  of the RDD interface, that it takes as argument a lambda function that calculate the normlized version of the current element of dataset.
+         * The normalization function is equal to x-min/max-min, where x is the element of the dataset that have to be normalized, min is the minimal value of dataset and max is the maximum.
+         * Thus, before to call the map function, we have used min and max method of the RDD interface to retrive the minimal and maximal value of the whole dataset.
          *
          */
-        final Double max = dNumbers.max(new DoubleComparator());
-        final Double min = dNumbers.min(new DoubleComparator());
+        final Double lMax = dNumbers.max(comparator);
+        final Double lMin = dNumbers.min(comparator);
 
-        JavaRDD<Double> dNormalizedNumbers = dNumbers.map((x) -> (x-min)/(max-min));
-        //dNormalizedNumbers.collect().stream().forEach(x -> System.out.println(x)); --> TODO: de-comment if you want to see the normalization
+        JavaRDD<Double> dNormalizedNumbers = dNumbers.map((x) -> (x-lMin)/(lMax-lMin));
+        /**
+         * These values are the normalized values
+         */
+        dNormalizedNumbers.collect().stream().forEach(x -> System.out.println(x));
 
 
         /**
-         * STEP 3 getting a "statistics of our choiche over the dNomralized data
-         * median:
-         * mean:
+         * STEP 3 get statistics of our choiche over the dNomralized data
          *
+         * As statistics, we have choose the median and the mean.
+         * In order to work with the first we have use four methods of the RDD interface:
+         * + sortBy: necessary to sort the elements
+         * + zipWithIndex: necessary to map each element of the dataset to a tuple that contains the element and its index in the dataset.
+         * + count: necessary to count the number of elements in the dataset.
+         * + filter: necessary to filters the element by their index.
+         *
+         * Once we have ordered and zipped the elements  with their index, we have calculated the index of the median by counting  the elements and by checking if the number of elements was odd or even.
+         * After that we have filtered the element by checking if their index was equal to the median index. At this point we have used the first method to retrieve the number with the position equals to the index of the median.
+         * N.B: The real index of the median is equal to lMedianIndex-1 since the first index of the zipped RDD is 0 instead of 1 in agreement with the fact that in computer science 0 is a number and not just a representation.
+         *
+         * About the mean, we have used a reduce function that take care about the sum of all elements of the dataset.
+         * Then, we have divided the results for the number of the elements that have been calculated through the usage of the count method of the RDD interface.
          */
 
-        //Mediana
+        //Median
+
         dNormalizedNumbers = dNormalizedNumbers.sortBy(x -> x, true, dNormalizedNumbers.partitions().size());
-        long numberOfElements = dNormalizedNumbers.count();
-        final long medianIndex = numberOfElements % 2 == 0? numberOfElements / 2: numberOfElements+1 /2;
-        System.out.println("Median of the values: "+String.valueOf(dNormalizedNumbers.zipWithIndex().filter(tuple -> tuple._2 == medianIndex).first()._1()));
+        long lNumberOfElements = dNormalizedNumbers.count();
+        final long lMedianIndex = lNumberOfElements % 2 == 0? lNumberOfElements / 2: lNumberOfElements+1 /2;
 
-        //Media
+        /**
+         * This value is the median value
+         */
+        System.out.println("The median of the normalized values is: "+String.valueOf(dNormalizedNumbers.zipWithIndex().filter(tuple -> tuple._2 == lMedianIndex-1).first()._1()));
 
+        //Mean
+        double lMean = (dNormalizedNumbers.reduce((x,y) -> x+y)) / dNormalizedNumbers.count();
+        /**
+         * This value is the mean value
+         */
+        System.out.println("The mean of the normalized values is: " + lMean);
     }
 
-    // It is important to mark this class as `static`.
+
     public static class DoubleComparator implements Serializable, Comparator<Double> {
 
         public int compare(Double a, Double b) {
