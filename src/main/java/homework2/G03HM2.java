56d13abb02d94423f7b790240cc577932f69ea68
stefano_campese_omnys <campydj90133>
2019-04-12 10:04:37 +0200

    fix and update for second assignment

--- src/main/java/homework2/G03HM2.java
+++ src/main/java/homework2/G03HM2.java
@@ -105,8 +105,11 @@ public class G03HM2 /*implements CommonMethodsInterfac*/ {
 
         List<JavaPairRDD<String, Long>> wordCounts = new ArrayList<>();
 
+        /**
+         * this isImproved Word count 1 algorithm described in class the using reduceByKey method to compute the final counts
+         */
         JavaPairRDD<String, Long> wordCount1 = docs
-                // Map phase
+                // Map phase: each document is mapped to its list of words paired with their occurencies: (w, ci(w))
                 .flatMapToPair((PairFlatMapFunction<String, String, Long>) document -> {
                     String[] tokens = document.split(" ");
                     HashMap<String, Long> counts = new HashMap<>();
@@ -120,14 +123,20 @@ public class G03HM2 /*implements CommonMethodsInterfac*/ {
 
                     return pairs.iterator();
 
-                })//reduce phase
+                })//Reduce phase: for each pair (w,ci(w)) where w is the same word, we reduce it to (w, c(w)) where w is the analyzed word and c(w) corresponds to sum of all eht occurencies of w in each document.
+                // In other words, c(w) is equal to the summatory of all ci(w) by key w
                 .reduceByKey(reduceFunction);
 
         wordCounts.add(wordCount1);
 
-
+        /**
+         * This is the Word count 2a:
+         * a variant of the algorithm presented in class where random keys take K possible values, where K is the value given in input.
+         * The easiest thing to do is to assing random integers between 0 and K-1. The algorithm presented in class had K=N^(1/2).
+         * You must use method groupBy to assign random keys to pairs and to group the pairs based on the assigned keys as required in the first round of the algorithm.
+         */
         JavaPairRDD<String, Long> wordCount2a = docs
-                // Map phase
+                // Map phase: each document is mapped to its list of words paired with their occurencies: (w, ci(w))
                 .flatMapToPair((document) -> {
                     String[] tokens = document.split(" ");
                     HashMap<String, Long> counts = new HashMap<>();
@@ -140,7 +149,12 @@ public class G03HM2 /*implements CommonMethodsInterfac*/ {
                     }
                     return pairs.iterator();
                 })
-                .groupBy(randomKAssignment)
+                .groupBy(randomKAssignment) // Using the group by we group each (w,ci(w)) by a random key k € [0, K-1] where K is given in input.
+
+                /**
+                 *  For all (x,(w,ci(w))), that have been generated in the previous step, of group x, we create a (w,c(x,w)) for each word w.
+                 *  where c(x,w) corresponds to the summatory of (x,(w,ci(w))).
+                 *  */
                 .flatMapToPair((PairFlatMapFunction<Tuple2<Integer, Iterable<Tuple2<String, Long>>>, String, Long>) element -> {
                     //for the each element we create its list of pairs (w, c(w))
                     Map<String, List<Tuple2<String, Long>>> groupedTuple = StreamSupport.stream(element._2.spliterator(), false)
@@ -152,7 +166,7 @@ public class G03HM2 /*implements CommonMethodsInterfac*/ {
                      */
                     final List<Tuple2<String, Long>> list = new ArrayList<>();
                     groupedTuple.forEach((k, v) -> {
-                        Tuple2 tmpTuple = v.stream().reduce((t1, t2) -> new Tuple2<String, Long>(k, t1._2 + t2._2)).orElse(null);
+                        Tuple2 tmpTuple = v.stream().reduce((t1, t2) -> new Tuple2<String, Long>(k, t1._2 + t2._2)).orElse(new Tuple2<String, Long>("", 0L));
                         if (tmpTuple != null) {
                             list.add(tmpTuple);
                         }
@@ -160,13 +174,21 @@ public class G03HM2 /*implements CommonMethodsInterfac*/ {
                     });
 
                     return list.iterator();
-                })//round2
+                })//Reduce phase: for each pair (w,ci(w)) where w is the same word, we reduce it to (w, c(w)) where w is the analyzed word and c(w) corresponds to sum of all eht occurencies of w in each document.
+                // In other words, c(w) is equal to the summatory of all ci(w) by key w
                 .reduceByKey(reduceFunction);
 
         wordCounts.add(wordCount2a);
 
+        /**
+         * This is the Word count 2b:
+         * a variant that does not explicitly assign random keys but exploits the subdivision of docs into K parts in combination with mapPartitionToPair to access each partition separately.
+         * Again, K is the value given in input. Note that if docs was initially partitioned into K parts, then, even after transformations that act on individual elements,
+         * the resulting RDD stays partitioned into K parts and you may exploit this partition. However, you can also invoke repartition(K) to reshuffle the RDD elements at random.
+         * Do whatever you think it yields better performance.
+         */
         JavaPairRDD<String, Long> wordCount2b = docs
-                // Map phase
+                // Map phase: each document is mapped to its list of words paired with their occurencies: (w, ci(w))
                 .flatMapToPair((document) -> {
                     String[] tokens = document.split(" ");
                     HashMap<String, Long> counts = new HashMap<>();
@@ -178,7 +200,12 @@ public class G03HM2 /*implements CommonMethodsInterfac*/ {
                         pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
                     }
                     return pairs.iterator();
-                }).mapPartitionsToPair(tuple2Iterator -> {
+
+                }) /**
+         * Similar to the varian a, for all (x,(w,ci(w))), that have been generated in the previous step, of group x, we create a (w,c(x,w)) for each word w.
+         *  where c(x,w) corresponds to the summatory of (x,(w,ci(w))).
+         **/
+                .mapPartitionsToPair(tuple2Iterator -> {
 
                     //for the each element we create its list of pairs (w, c(w))
                     Iterable<Tuple2<String, Long>> splitIterator = () -> tuple2Iterator;
@@ -188,14 +215,15 @@ public class G03HM2 /*implements CommonMethodsInterfac*/ {
                     final List<Tuple2<String, Long>> list = new ArrayList<>();
 
                     groupedTuple.forEach((k, v) -> {
-                        Tuple2 tmpTuple = v.stream().reduce((tuple1, tuple2) -> new Tuple2<>(k, tuple1._2 + tuple2._2)).orElse(null);
+                        Tuple2 tmpTuple = v.stream().reduce((tuple1, tuple2) -> new Tuple2<>(k, tuple1._2 + tuple2._2)).orElse(new Tuple2<String, Long>("", 0L));
                         if (tmpTuple != null) {
                             list.add(tmpTuple);
                         }
                     });
                     return list.iterator();
 
-                })
+                })//Reduce phase: for each pair (w,ci(w)) where w is the same word, we reduce it to (w, c(w)) where w is the analyzed word and c(w) corresponds to sum of all eht occurencies of w in each document.
+                // In other words, c(w) is equal to the summatory of all ci(w) by key w
                 .reduceByKey(reduceFunction);
 
         wordCounts.add(wordCount2b);
@@ -203,13 +231,25 @@ public class G03HM2 /*implements CommonMethodsInterfac*/ {
         for (int i = 0; i < wordCounts.size(); i++) {
             JavaPairRDD<String, Long> wc = wordCounts.get(i);
             Instant instant = Instant.now();
-            wc.count(); // due to execute all (lazy)
+            //due to the lazy execution approaches of Spark, we have to trigger the computation in order to measure the real execution timing
+            wc.count();
             results.add(String.format("Execution for method %d is: %s milliseconds", i + 1, MILLIS.between(instant, Instant.now())));
             double averageLenght = wc.keys().map(word -> word.length()).reduce((x, y) -> x + y) / (double) wc.count();
             results.add(String.format("Average length for method %d is: %f", i + 1, averageLenght));
         }
 
+        // we iterate over the string results and print them (both timing and average length)
         results.forEach(result -> System.out.println(result));
+
+        /**
+         * FINAL CONSIDERATIONS:
+         * + Using repartition too frequently is not always useful. It could introduces overhead, specially when the Spark context is deployed on a cluster with sever workers (even worse when those workers are have different computation power, or when
+         * the network performances between the nodes are not optimal)
+         *
+         * + Repartition seems to increase the performances, but the number of partitions seems to be correlated to the number of the workers and by dataset dimension
+         *
+         * + Performances are also correlated to lambda functions that are used by each step: Using Java 8 statements, like stream or reduce, can optimize the execution, computation time and reduce the algorithm complexity.
+         */
 
     }
 }
